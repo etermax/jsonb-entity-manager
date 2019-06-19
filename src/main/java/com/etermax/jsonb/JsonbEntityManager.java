3a3167babe0d818448fa9b8c25939af84b582058
alejandra <alejandra.juarez@etermax.com>
2019-06-19 15:53:55 -0300

    remove javadocs and refactor to remove duplicated code

--- src/main/java/com/etermax/jsonb/JsonbEntityManager.java
+++ src/main/java/com/etermax/jsonb/JsonbEntityManager.java
@@ -1,215 +1,151 @@
 package com.etermax.jsonb;
 
+import com.etermax.jsonb.connection.PostgresConnector;
+import com.etermax.jsonb.tablenames.TableNamesResolver;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Optional;
+
 import static com.etermax.jsonb.exceptions.ExceptionCatcher.executeOrRuntime;
 import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 
-import java.util.List;
-import java.util.Optional;
-
-import com.etermax.jsonb.connection.PostgresConnector;
-import com.etermax.jsonb.tablenames.TableNamesResolver;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @SuppressWarnings("rawtypes")
 public class JsonbEntityManager {
-	public static final String ENTITY_FIELD = "entity";
-	private ObjectMapper objectMapper;
-	private PostgresConnector connector;
-	private TableNamesResolver tableNamesResolver;
+    public static final String ENTITY_FIELD = "entity";
+    private ObjectMapper objectMapper;
+    private PostgresConnector connector;
+    private TableNamesResolver tableNamesResolver;
 
-	public JsonbEntityManager(ObjectMapper objectMapper, PostgresConnector connector, TableNamesResolver tableNamesResolver) {
-		this.objectMapper = objectMapper;
-		this.connector = connector;
-		this.tableNamesResolver = tableNamesResolver;
-	}
+    public JsonbEntityManager(ObjectMapper objectMapper, PostgresConnector connector, TableNamesResolver tableNamesResolver) {
+        this.objectMapper = objectMapper;
+        this.connector = connector;
+        this.tableNamesResolver = tableNamesResolver;
+    }
 
-	/**
-	 * <p>Persist the entity. If it is new, then it will assign an id reading it from de corresponding sequence:
-	 * <br>(the annotated table name with the prefix "seq_")
-	 * </p>
-	 *
-	 * @param entity the JsonbEntity to persist
-	 * @return the list with all matching objects
-	 */
-	public void save(JsonbEntity entity) {
-		if (!entity.persisted()) {
-			assignIdToNewEntity(entity);
-			String saveQuery = "INSERT INTO %s (id, entity) VALUES (%s, '%s');";
-			connector.execute(format(saveQuery, getTableName(entity.getClass()), entity.getId(), serialize(entity)));
-		} else {
-			String updateQuery = "update %s set entity= '%s' where id = %s;";
-			connector.execute(format(updateQuery, getTableName(entity.getClass()), serialize(entity), entity.getId()));
-		}
-	}
+    public void save(JsonbEntity entity) {
+        if (!entity.persisted()) {
+            assignIdToNewEntity(entity);
+            String saveQuery = "INSERT INTO %s (id, entity) VALUES (%s, '%s');";
+            connector.execute(format(saveQuery, getTableName(entity.getClass()), entity.getId(), serialize(entity)));
+        } else {
+            String updateQuery = "update %s set entity= '%s' where id = %s;";
+            connector.execute(format(updateQuery, getTableName(entity.getClass()), serialize(entity), entity.getId()));
+        }
+    }
 
-	/**
-	 * <p>Finds a list of entities for the query.
-	 * </p>
-	 *
-	 * @param clazz The class of the objecct that will be deleted
-	 * @param query the query to retrieve the JsonbEntities
-	 * @return the list with all matching objects
-	 */
-	public <T extends JsonbEntity> List<T> findListEntityResult(Class<T> clazz, String query) {
-		List<String> jsons = newArrayList();
-		connector.execute(query, resultSet -> executeOrRuntime(() -> {
-			while (resultSet.next()) {
-				jsons.add(resultSet.getString(ENTITY_FIELD));
-			}
-		}));
-		return jsons.stream().map(json -> deserialize(clazz, json)).collect(toList());
-	}
+    public <T extends JsonbEntity> List<T> findListEntityResult(Class<T> clazz, String query) {
+        List<String> jsons = newArrayList();
+        connector.execute(query, resultSet -> executeOrRuntime(() -> {
+            while (resultSet.next()) {
+                jsons.add(resultSet.getString(ENTITY_FIELD));
+            }
+        }));
+        return jsons.stream().map(json -> deserialize(clazz, json)).collect(toList());
+    }
 
-	/**
-	 * <p>Finds a list of entities for the query on the writing node. <br>
-	 * Use it just when you can't wait for the propagation time over the reading nodes, but be careful about the load.
-	 * </p>
-	 *
-	 * @param clazz The class of the objecct that will be deleted
-	 * @param query the query to retrieve the JsonbEntities
-	 * @return the list with all matching objects
-	 */
-	public <T extends JsonbEntity> List<T> findListEntityResultOnWriteNode(Class<T> clazz, String query) {
-		List<String> jsons = newArrayList();
-		connector.executeOnWriteNode(query, rs -> executeOrRuntime(() -> {
-			while (rs.next()) {
-				jsons.add(rs.getString(ENTITY_FIELD));
-			}
-		}));
-		return jsons.stream().map(json -> deserialize(clazz, json)).collect(toList());
-	}
+    public <T extends JsonbEntity> List<T> findListEntityResultOnWriteNode(Class<T> clazz, String query) {
+        List<String> jsons = newArrayList();
+        connector.executeOnWriteNode(query, rs -> executeOrRuntime(() -> {
+            while (rs.next()) {
+                jsons.add(rs.getString(ENTITY_FIELD));
+            }
+        }));
+        return jsons.stream().map(json -> deserialize(clazz, json)).collect(toList());
+    }
 
-	/**
-	 * <p>Deletes from data base the persisted instance with that id
-	 * </p>
-	 *
-	 * @param clazz The class of the objecct that will be deleted
-	 * @param id    the assigned id on save
-	 */
-	public void delete(Class<?> clazz, Long id) {
-		connector.execute(format("delete from %s where id=%d", getTableName(clazz), id));
-	}
+    public void delete(Class<?> clazz, Long id) {
+        connector.execute(format("delete from %s where id=%d", getTableName(clazz), id));
+    }
 
-	/**
-	 * <p>This method search a unique JsonbEntity.
-	 * </p>
-	 *
-	 * @param clazz The class that will be deserialized to the parametrized type
-	 * @param query The query to find a unique instance for your object
-	 * @return one unique result for the class
-	 */
-	public <T extends JsonbEntity> Optional<T> findUniqueEntityResult(Class<T> clazz, String query) {
-		Retriever<String> retriever = new Retriever<>();
-		connector.execute(query, rs -> executeOrRuntime(() -> {
-			if (rs.next()) {
-				retriever.set(rs.getString(ENTITY_FIELD));
-			}
-		}));
-		return ofNullable(retriever.get()).map(element -> deserialize(clazz, element));
-	}
+    public <T extends JsonbEntity> Optional<T> findUniqueEntityResult(Class<T> clazz, String query) {
+        Retriever<String> retriever = new Retriever<>();
+        connector.execute(query, rs -> executeOrRuntime(() -> {
+            if (rs.next()) {
+                retriever.set(rs.getString(ENTITY_FIELD));
+            }
+        }));
+        return ofNullable(retriever.get()).map(element -> deserialize(clazz, element));
+    }
 
-	/**
-	 * <p>This method search a unique JsonbEntity on the writing node. <br>
-	 * Use it just when you can't wait for the propagation time over the reading nodes, but be careful about the load.
-	 * </p>
-	 *
-	 * @param clazz The class that will be deserialized to the parametrized type
-	 * @param query The query to find a unique instance for your object
-	 * @return one unique result for the class
-	 */
-	public <T extends JsonbEntity> Optional<T> findUniqueEntityResultOnWriteNode(Class<T> clazz, String query) {
-		Retriever<String> retriever = new Retriever<>();
-		connector.executeOnWriteNode(query, rs -> executeOrRuntime(() -> {
-			if (rs.next()) {
-				retriever.set(rs.getString(ENTITY_FIELD));
-			}
-		}));
-		return ofNullable(retriever.get()).map(element -> deserialize(clazz, element));
-	}
+    public <T extends JsonbEntity> Optional<T> findUniqueEntityResultOnWriteNode(Class<T> clazz, String query) {
+        Retriever<String> retriever = new Retriever<>();
+        connector.executeOnWriteNode(query, rs -> executeOrRuntime(() -> {
+            if (rs.next()) {
+                retriever.set(rs.getString(ENTITY_FIELD));
+            }
+        }));
+        return ofNullable(retriever.get()).map(element -> deserialize(clazz, element));
+    }
 
-	/**
-	 * <p>This method retrieves all the entities for the parametrized class
-	 * </p>
-	 *
-	 * @param clazz The class that will be deserialized to the parametrized type
-	 * @return the list with all persisted objects
-	 */
-	public <T extends JsonbEntity> List<T> findAll(Class<T> clazz) {
-		String query = "SELECT * FROM %s;";
-		return findListEntityResult(clazz, format(query, getTableName(clazz)));
-	}
+    public <T extends JsonbEntity> List<T> findAll(Class<T> clazz) {
+        String query = "SELECT * FROM %s;";
+        return findListEntityResult(clazz, format(query, getTableName(clazz)));
+    }
 
-	/**
-	 * <p>Finds a persisted instance by the assigned id
-	 * </p>
-	 *
-	 * @param clazz The class that will be deserialized to the parametrized type
-	 * @param id    the assigned id on save
-	 * @return an optional of the object. It will be empty if the id is wrong
-	 */
-	public <T extends JsonbEntity> Optional<T> findById(Class<T> clazz, long id) {
-		String saveQuery = "select id, entity from %s where id = %d;";
-		return findUniqueEntityResult(clazz, format(saveQuery, getTableName(clazz), id));
-	}
+    public <T extends JsonbEntity> Optional<T> findById(Class<T> clazz, long id) {
+        String saveQuery = "select id, entity from %s where id = %d;";
+        return findUniqueEntityResult(clazz, format(saveQuery, getTableName(clazz), id));
+    }
 
-	public <T> T findPrimitiveResult(String query) {
-		Retriever<T> retriever = new Retriever<>();
-		connector.execute(query, resultSet -> executeOrRuntime(() -> {
-			if (resultSet.next()) {
-				retriever.set((T) resultSet.getObject(1));
-			}
-		}));
-		return retriever.get();
-	}
+    public <T> T findPrimitiveResult(String query) {
+        Retriever<T> retriever = new Retriever<>();
+        connector.execute(query, resultSet -> executeOrRuntime(() -> {
+            if (resultSet.next()) {
+                retriever.set((T) resultSet.getObject(1));
+            }
+        }));
+        return retriever.get();
+    }
 
-	public <T> List<T> findPrimitiveListResult(String query) {
-		List<T> list = newArrayList();
-		connector.execute(query, rs -> executeOrRuntime(() -> {
-			while (rs.next()) {
-				list.add((T) rs.getObject(1));
-			}
-		}));
-		return list;
-	}
+    public <T> List<T> findPrimitiveListResult(String query) {
+        List<T> list = newArrayList();
+        connector.execute(query, rs -> executeOrRuntime(() -> {
+            while (rs.next()) {
+                list.add((T) rs.getObject(1));
+            }
+        }));
+        return list;
+    }
 
-	private <T> T deserialize(Class<T> clazz, String json) {
-		return executeOrRuntime(() -> objectMapper.readValue(json, clazz));
-	}
+    private <T> T deserialize(Class<T> clazz, String json) {
+        return executeOrRuntime(() -> objectMapper.readValue(json, clazz));
+    }
 
-	private void assignIdToNewEntity(JsonbEntity entity) {
-		connector.executeNextVal(format("select nextval('%s')", getSequenceName(entity.getClass())), rs -> executeOrRuntime(() -> {
-			rs.next();
-			entity.setId(rs.getLong(1));
-		}));
-	}
+    private void assignIdToNewEntity(JsonbEntity entity) {
+        connector.executeNextVal(format("select nextval('%s')", getSequenceName(entity.getClass())), rs -> executeOrRuntime(() -> {
+            rs.next();
+            entity.setId(rs.getLong(1));
+        }));
+    }
 
-	private String serialize(JsonbEntity entity) {
-		return executeOrRuntime(() -> objectMapper.writeValueAsString(entity).replace("'", "''"));
-	}
+    private String serialize(JsonbEntity entity) {
+        return executeOrRuntime(() -> objectMapper.writeValueAsString(entity).replace("'", "''"));
+    }
 
-	private String getTableName(Class<?> clazz) {
-		return tableNamesResolver.getTableName(clazz);
-	}
+    private String getTableName(Class<?> clazz) {
+        return tableNamesResolver.getTableName(clazz);
+    }
 
-	private String getSequenceName(Class<?> clazz) {
-		return tableNamesResolver.getSequenceName(clazz);
-	}
+    private String getSequenceName(Class<?> clazz) {
+        return tableNamesResolver.getSequenceName(clazz);
+    }
 
-	public class Retriever<J> {
+    private class Retriever<J> {
 
-		private J element;
+        private J element;
 
-		public J get() {
-			return element;
-		}
+        public J get() {
+            return element;
+        }
 
-		public void set(J element) {
-			this.element = element;
-		}
+        public void set(J element) {
+            this.element = element;
+        }
 
-	}
+    }
 
 }
--- src/main/java/com/etermax/jsonb/connection/PostgresConnector.java
+++ src/main/java/com/etermax/jsonb/connection/PostgresConnector.java
@@ -1,7 +1,10 @@
 package com.etermax.jsonb.connection;
 
-import static com.etermax.jsonb.exceptions.ExceptionCatcher.executeOrRuntime;
-import static java.lang.String.format;
+import com.etermax.jsonb.exceptions.PostgresConnectionException;
+import com.zaxxer.hikari.HikariDataSource;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,107 +13,99 @@ import java.sql.Statement;
 import java.time.ZonedDateTime;
 import java.util.function.Consumer;
 
-import org.joda.time.DateTime;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.etermax.jsonb.exceptions.PostgresConnectionException;
-import com.zaxxer.hikari.HikariDataSource;
+import static com.etermax.jsonb.exceptions.ExceptionCatcher.executeOrRuntime;
+import static java.lang.String.format;
 
 public class PostgresConnector {
-	private static final Logger logger = LoggerFactory.getLogger(PostgresConnector.class);
-	public static final String ERROR = "ERROR PG Executing: ";
-	private static final String LOG_MESSAGE = "%s time: %d %s PG Executing: %s";
-	private HikariDataSource readDataSource;
-	private HikariDataSource writeDataSource;
-	private int queryTimeout;
-	private String url;
+    private static final Logger logger = LoggerFactory.getLogger(PostgresConnector.class);
+    public static final String ERROR = "ERROR PG Executing: ";
+    private static final String LOG_MESSAGE = "%s time: %d %s PG Executing: %s";
+    private HikariDataSource readDataSource;
+    private HikariDataSource writeDataSource;
+    private int queryTimeout;
+    private String url;
 
-	public PostgresConnector(HikariDataSource dataSource) {
-		this(dataSource, dataSource, 1, "");
-	}
+    public PostgresConnector(HikariDataSource dataSource) {
+        this(dataSource, dataSource, 1, "");
+    }
 
-	public PostgresConnector(HikariDataSource readDataSource, HikariDataSource writeDataSource) {
-		this(readDataSource, writeDataSource, 1, "");
-	}
+    public PostgresConnector(HikariDataSource readDataSource, HikariDataSource writeDataSource) {
+        this(readDataSource, writeDataSource, 1, "");
+    }
 
-	public PostgresConnector(HikariDataSource readDataSource, HikariDataSource writeDataSource, int queryTimeout, String url) {
-		this.readDataSource = readDataSource;
-		this.writeDataSource = writeDataSource;
-		this.queryTimeout = queryTimeout;
-		this.url = url;
-	}
+    public PostgresConnector(HikariDataSource readDataSource, HikariDataSource writeDataSource, int queryTimeout, String url) {
+        this.readDataSource = readDataSource;
+        this.writeDataSource = writeDataSource;
+        this.queryTimeout = queryTimeout;
+        this.url = url;
+    }
 
-	public void execute(String query, Consumer<ResultSet> consumer) {
-		long initialTime = ZonedDateTime.now().toInstant().toEpochMilli();
-		try (Connection connection = readDataSource.getConnection(); Statement statement = connection.createStatement()) {
-			statement.setQueryTimeout(queryTimeout);
-			try (ResultSet rs = statement.executeQuery(query)) {
-				consumer.accept(rs);
-			}
-		} catch (Exception e) {
-			logError(query, e);
-			throw new PostgresConnectionException(e);
-		}
-		long finalTime = ZonedDateTime.now().toInstant().toEpochMilli();
-		logInfo((finalTime - initialTime), "read", "read datasource", query);
+    public void execute(String query, Consumer<ResultSet> consumer) {
+        long initialTime = ZonedDateTime.now().toInstant().toEpochMilli();
+        doExecuteRead(query, consumer, readDataSource);
+        long finalTime = ZonedDateTime.now().toInstant().toEpochMilli();
+        logInfo((finalTime - initialTime), "read", "read datasource", query);
 
-	}
+    }
 
-	public void execute(String query) {
-		long initialTime = ZonedDateTime.now().toInstant().toEpochMilli();
-		try (Connection conn = writeDataSource.getConnection(); PreparedStatement statement = conn.prepareStatement(query)) {
-			statement.setQueryTimeout(queryTimeout);
-			statement.executeUpdate();
-		} catch (Exception e) {
-			logError(query, e);
-			throw new PostgresConnectionException(e);
-		}
-		long finalTime = ZonedDateTime.now().toInstant().toEpochMilli();
-		logInfo((finalTime - initialTime), "write", "write datasource", query);
-	}
+    public void execute(String query) {
+        long initialTime = ZonedDateTime.now().toInstant().toEpochMilli();
+        try (Connection conn = writeDataSource.getConnection(); PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setQueryTimeout(queryTimeout);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            logError(query, e);
+            throw new PostgresConnectionException(e);
+        }
+        long finalTime = ZonedDateTime.now().toInstant().toEpochMilli();
+        logInfo((finalTime - initialTime), "write", "write datasource", query);
+    }
 
-	public void executeOnWriteNode(String query, Consumer<ResultSet> consumer) {
-		long initialTime = DateTime.now().getMillis();
-		try (Connection conn = writeDataSource.getConnection(); Statement st = conn.createStatement()) {
-			st.setQueryTimeout(queryTimeout);
-			try (ResultSet rs = st.executeQuery(query)) {
-				consumer.accept(rs);
-			}
-		} catch (Exception e) {
-			logError(query, e);
-			throw new PostgresConnectionException(e);
-		}
-		long finalTime = DateTime.now().getMillis();
-		logInfo((finalTime - initialTime), "read", "write datasource", query);
+    public void executeOnWriteNode(String query, Consumer<ResultSet> consumer) {
+        long initialTime = DateTime.now().getMillis();
+        doExecuteRead(query, consumer, writeDataSource);
+        long finalTime = DateTime.now().getMillis();
+        logInfo((finalTime - initialTime), "read", "write datasource", query);
 
-	}
+    }
 
-	private void logInfo(long time, String opperationType, String overDataSource, String query) {
-		String info = format(LOG_MESSAGE, opperationType, time, overDataSource, query);
-		logger.info(info);
-	}
+    private void doExecuteRead(String query, Consumer<ResultSet> consumer, HikariDataSource dataSource) {
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+            statement.setQueryTimeout(queryTimeout);
+            try (ResultSet rs = statement.executeQuery(query)) {
+                consumer.accept(rs);
+            }
+        } catch (Exception e) {
+            logError(query, e);
+            throw new PostgresConnectionException(e);
+        }
+    }
 
-	private void logError(String query, Exception e) {
-		String error = ERROR + query;
-		logger.error(error, e);
-	}
+    private void logInfo(long time, String opperationType, String overDataSource, String query) {
+        String info = format(LOG_MESSAGE, opperationType, time, overDataSource, query);
+        logger.info(info);
+    }
 
-	public void executeNextVal(String query, Consumer<ResultSet> consumer) {
-		executeOnWriteNode(query, consumer);
-	}
+    private void logError(String query, Exception e) {
+        String error = ERROR + query;
+        logger.error(error, e);
+    }
 
-	public void executeExist(String query, Consumer<Boolean> consumer) {
-		execute(query, rs -> executeOrRuntime(() -> {
-			rs.next();
-			consumer.accept(rs.getBoolean(1));
-		}));
-	}
+    public void executeNextVal(String query, Consumer<ResultSet> consumer) {
+        executeOnWriteNode(query, consumer);
+    }
 
-	public void executeCount(String query, Consumer<Long> consumer) {
-		execute(query, rs -> executeOrRuntime(() -> {
-			rs.next();
-			consumer.accept(rs.getLong(1));
-		}));
-	}
+    public void executeExist(String query, Consumer<Boolean> consumer) {
+        execute(query, rs -> executeOrRuntime(() -> {
+            rs.next();
+            consumer.accept(rs.getBoolean(1));
+        }));
+    }
+
+    public void executeCount(String query, Consumer<Long> consumer) {
+        execute(query, rs -> executeOrRuntime(() -> {
+            rs.next();
+            consumer.accept(rs.getLong(1));
+        }));
+    }
 }
